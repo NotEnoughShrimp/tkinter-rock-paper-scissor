0b5eb6b144f4e9c77d077c17371b8d9351288c94
HowManyShrimp <kim_yonghui@outlook.com>
2023-12-12 15:55:36 -0500

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -19,17 +19,32 @@ def game_function(player_choice):
         canvas.create_image(0,100, anchor=tkinter.NW, image=scissor_p)
 
     if computer_choice == 1:
-        canvas.create_image(800,100, anchor=tkinter.NE, image=rock_c)
+        canvas.create_image(500,100, anchor=tkinter.NW, image=rock_c)
     elif computer_choice == 2:
-        canvas.create_image(800,100, anchor=tkinter.NE, image=paper_c)
+        canvas.create_image(500,100, anchor=tkinter.NW, image=paper_c)
     elif computer_choice == 3:
-        canvas.create_image(800,100, anchor=tkinter.NE, image=scissor_c)
+        canvas.create_image(500,100, anchor=tkinter.NW, image=scissor_c)
+    
+    if player_choice == computer_choice:
+        result = "draw"
+    elif (player_choice==1 and computer_choice==3) or (player_choice == 2 and computer_choice == 1) or (player_choice == 3 and computer_choice == 2):
+        result = "you win"
+    else:
+        result = " you lose"
+    
+    canvas.create_text(390,600, text="Result:" + result, fill="black", font=("Algerian",25), tag="result")
+    
+def clear():
+    canvas.delete("result")
+    canvas.create_image(0, 100, anchor=tkinter.NW, image=img_p)
+    canvas.create_image(500, 100, anchor=tkinter.NW, image=img_c)
+    
 
 root = tkinter.Tk()  # initializes tkinter
 root.title("Rock Paper Scissors")  # sets what appears on window
 root.geometry("800x680")  # sets window dimensions
 canvas = tkinter.Canvas(root, width=800, height=680)
-canvas.pack()
+canvas.grid(row=0, column=0)
 
 """ labels for the gui"""
 player_label = tkinter.Label(root, text="Player", font=("Algerian",25))
@@ -81,18 +96,24 @@ scissor_c = ImageTk.PhotoImage(scissor_flipped_image)
 selection_image = Image.open('rps.jpg')
 resized_selection = selection_image.resize((300,130))
 selection = ImageTk.PhotoImage(resized_selection)
-canvas.create_image(400,500, anchor=tkinter.CENTER, image=selection)
 
+
+canvas.create_image(0, 100, anchor=tkinter.NW, image=img_p)
+canvas.create_image(500, 100, anchor=tkinter.NW, image=img_c)
+canvas.create_image(0, 400, anchor=tkinter.NW, image=selection)
+canvas.create_image(500, 400, anchor=tkinter.NW, image=selection)
 
 # Without Lambda it just runs the function and draws the image. WITH lambda, it waits for the click
 rock_button = tkinter.Button(root, text="Rock", command=lambda:game_function(1))
 paper_button = tkinter.Button(root, text="Paper", command=lambda:game_function(2))
 scissor_button = tkinter.Button(root, text="Scissor", command=lambda:game_function(3))
 
-rock_button.place(x=100, y=340)
-paper_button.place(x=400, y=340)
-scissor_button.place(x=700, y=340)
+rock_button.place(x=35, y=487)
+paper_button.place(x=128, y=487)
+scissor_button.place(x=220, y=487)
 
+clear_b = tkinter.Button(root, text='CLEAR', font=('Times', 10, 'bold'),
+                 width=10, command=clear).place(x=370, y=28)
 # """ Part 2: Game Implementation"""
 # def game_function(player_choice):
 #     values = [1,2,3]
